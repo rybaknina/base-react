eb2be996868e9a351d8e54fdf9dd195630e98e06
Nina Rybak <nrybak@dev.vtb.ru>
2022-03-27 20:53:18 +0300

    task-1: App рендерит Message с переданным текстом

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
+import Message from "./Message";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo incidunt minima neque velit? Adipisci consequatur dicta, dolor eligendi, in ipsam magni nihil numquam pariatur porro quos repellendus sint tempore veritatis?";
+    return (
+        <div className="App">
+            <Message text={text}/>
+        </div>
+    );
 }
 
 export default App;
